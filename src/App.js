906a1291bafc49a45e2025f111d8f3c830054611
martin jerez <martincho111986@gmail.com>
2022-04-30 16:35:22 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [year, setYear] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [textSearch, setTextSearch] = useState(
+    "Buscar pelicula peliculas por Año"
+  );
+
+  const handleChange = (e) => {
+    setYear(e.target.value * 1);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`https://jsonmock.hackerrank.com/api/moviesdata?Year=${year}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.data.length === 0) {
+          setMovies(data.data);
+          return setTextSearch("No Results Found");
+        }
+        setMovies(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form action="" className="form-container" onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          type="number"
+          name="search"
+          className="search-bar"
+          data-testid="app-input"
+        />
+        <button data-testid="submit-button" type="submit" className="search-btn">
+          Search
+        </button>
+      </form>
+      {movies.length === 0 ? (
+        <div className="no-data" data-testid="no-result">
+          {textSearch}
+        </div>
+      ) : (
+        <div className="results-container">
+          {movies.map((movie) => (
+            <ul key={movie.imdbID} data-testid="movieList">
+              <li>
+                {movie.Title} - {movie.Year}
+              </li>
+            </ul>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("should render an input", () => {
+    render(<App />);
+    const linkElement = screen.getByTestId("app-input");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("should render a button", () => {
+    render(<App />);
+    const linkElement = screen.getByTestId("submit-button");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("should render a div with no-data class", () => {
+    render(<App />);
+    const linkElement = screen.getByTestId("no-result");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("should render a ul with results if the API call is ok", () => {
+    render(<App />);
+    const linkElement = screen.queryByTestId("movieList");
+    expect(linkElement).not.toBeInTheDocument();
+  });
+ 
 });
